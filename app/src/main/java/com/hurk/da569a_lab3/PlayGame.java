dd9ad519a6a692c0d5ca92013839c30f2ed7e931
Hurkpaburk <johbe770@gmail.com>
2016-03-19 12:04:43 +0100

    click on bitmap added

--- app/src/main/java/com/hurk/da569a_lab3/PlayGame.java
+++ app/src/main/java/com/hurk/da569a_lab3/PlayGame.java
@@ -5,9 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -22,12 +24,12 @@ import java.util.HashMap;
 
 public class PlayGame extends Activity implements OnTouchListener {
 
-    Point touch = new Point(0, 0);
     protected boolean soundOn;
     protected Intent intent;
-    DrawView drawView;
-    SoundPool soundPool = null;
-    HashMap<Integer, Integer> soundPoolMap;
+    protected DrawView drawView;
+    protected int bitMapHeight, bitMapWidth, bitMapXStart, bitMapYStart;
+    protected MediaPlayer mp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +39,8 @@ public class PlayGame extends Activity implements OnTouchListener {
         soundOn = intent.getBooleanExtra(MainActivity.message, true);
         drawView = new DrawView(this);
         setContentView(drawView);
-
-        soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
-        soundPoolMap.put(0, soundPool.load(this, R.raw.sound, 1));
+        drawView.setOnTouchListener(this);
+        mp = MediaPlayer.create(this, R.raw.sound);
     }
 
     @Override
@@ -64,30 +65,23 @@ public class PlayGame extends Activity implements OnTouchListener {
         Bitmap monster = null;
         int frame = 0;
 
-
         public DrawView(Context context) {
             super(context);
+            surface = getHolder();
+            assetManager = context.getAssets();
             try {
-                surface = getHolder();
-                assetManager = context.getAssets();
                 InputStream inputStream = assetManager.open("blinking_green.png");
                 options = new BitmapFactory.Options();
                 options.inPreferredConfig = Bitmap.Config.ARGB_8888;
                 monster = BitmapFactory.decodeStream(inputStream, null, options);
+                bitMapHeight = monster.getHeight();
+                bitMapWidth = monster.getWidth();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        // CHANGE / REMOVE THIS!!!? ???
-        @Override
-        public void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            canvas.drawARGB(255, 154, 246, 240);
-            canvas.drawBitmap(monster, 400, 400, null);
-        }
+    }
 
-
-        public void resume() {
+    public void resume() {
             running = true;
             gameloop = new Thread(this);
             gameloop.start();
@@ -98,8 +92,7 @@ public class PlayGame extends Activity implements OnTouchListener {
             while (true) {
                 try {
                     gameloop.join();
-                }
-                catch (InterruptedException e) {
+                } catch (InterruptedException e) {
 
                 }
             }
@@ -107,20 +100,37 @@ public class PlayGame extends Activity implements OnTouchListener {
 
         @Override
         public void run() {
-            // SIDAN 148 kombinerat med onDraw ovan
+            while (running) {
+                if (!surface.getSurface().isValid()) continue;
+
+                Canvas canvas = surface.lockCanvas();
+                canvas.drawARGB(255, 154, 246, 240);
+                bitMapXStart = 400;
+                bitMapYStart = 400;
+                canvas.drawBitmap(monster, bitMapXStart, bitMapYStart, null);
+
+
+                surface.unlockCanvasAndPost(canvas);
+
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-
         int touchX = (int) event.getX();
         int touchY = (int) event.getY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (rect.contains(touchX, touchY)) { //Do something smart
+                if ( (touchX >= bitMapXStart && touchX <= bitMapXStart+bitMapWidth) &&
+                        (touchY >= bitMapYStart && touchY <= bitMapYStart+bitMapHeight )) {
                     if (soundOn) {
-                        playSound();
+                        mp.start();
                     } else {
                         // Do not play sound
                     }
@@ -129,13 +139,10 @@ public class PlayGame extends Activity implements OnTouchListener {
                     // Do nothing
                 }
                 break;
-            default:
+           default:
                 break;
         }
         return true;
     }
 
-    public void playSound() {
-        soundPool.play(0,1f,1f,1,1,1f);
-    }
 }
